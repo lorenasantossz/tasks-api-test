5a604abe8236a7aa9efac30fa6350e6880f045d8
lorenasantossz <lorenasantos.sz@hotmail.com>
2022-04-25 16:17:52 -0300

    Update APITest.java

--- src/test/java/tasks/apitest/APITest.java
+++ src/test/java/tasks/apitest/APITest.java
@@ -27,7 +27,7 @@ public class APITest {
 	@Test
 	public void deveAdicionarTarefaComSucesso() {
 		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2022-04-10\" }")
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2030-04-10\" }")
 			.contentType(ContentType.JSON)
 		.when()
 			.post("/todo")
